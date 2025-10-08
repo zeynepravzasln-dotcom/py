a9aea348d3210e274970516d7e674155a62b0e94
zeynepravzasln-dotcom <zeynepravzasln@gmail.com>
2025-10-08 17:16:16 +0300

    hesap_makine_commit_1

---
+++ python.py
@@ -0,0 +1,19 @@
+'''
+Zeynep Ravza ASLAN 
+zeynepravzasln@gmail.com 
+Hesap Makinesi Ödevi
+'''
+
+if __name__ =="__main__":
+
+    sayi1 = int(input("İlk sayıyı giriniz: "))
+    sayi2 = int(input("İkinci sayıyı giriniz: "))
+    toplam = sayi1+sayi2
+    cikarma = sayi1 - sayi2
+    carpma = sayi1 * sayi2
+    bolme = sayi1 / sayi2
+    print("Toplam: ", toplam)
+    print("Çıkarma: ",cikarma)
+    print("Çarpma: ",carpma)
+    print("Bölme: ",bolme)
+
