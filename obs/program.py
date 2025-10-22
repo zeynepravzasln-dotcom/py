f14b8296e38382923103afdd39c01178d71cd690
zeynepravzasln-dotcom <zeynepravzasln@gmail.com>
2025-10-22 13:31:26 +0300

    obs uygulaması

---
+++ obs/program.py
@@ -0,0 +1,11 @@
+'''
+yazar: zravza
+email: zeynepravzasln@gmail.com
+
+açıklama: python programlama dilini
+bir öğrenci bilgi sistemi senaryosu ile 
+öğreten repo (anaprogram dosyası)
+'''
+
+if __name__ == '__main__':
+    print("öğrenci bilgi sistemine hoş geldiniz:")
---
+++ untitled4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 13 17:41:46 2025
+
+@author: LENOVO
+"""
+
+for x in 'hoş geldin':
+    print(x)
+    print(x.upper())
+    print(x.lower())
+    print(x.strip())
+    print(x.replace("h","k"))
